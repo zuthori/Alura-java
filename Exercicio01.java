8f051315229401a1050896369b4f97696e3179ff
Pedro Alves <3pedru@gmail.com>
2016-12-05 10:32:01 -0200

    melhorias
    md para textos estilizados
    .java para arquivos java

---
+++ Exercicio01.java
@@ -0,0 +1,19 @@
+# Alura exercícios - Java
+
+public class Balancotrimestral {
+
+	public static void main(String[] args) {
+
+		int gastosJaneiro = 15000;
+
+		int gastosFevereiro = 23000;
+
+		int gastosMarco = 17000;
+
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+		System.out.println("Resultado trimestral: " + gastosTrimestre);
+
+	}
+
+}
