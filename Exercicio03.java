e9242777f764daa2e7faa0aa6654225a339d6018
Danilo Almeida <danilo00almeida@outlook.com>
2016-12-06 09:43:51 -0200

    While

---
+++ Exercicio03.java
@@ -0,0 +1,14 @@
+// Alura exercicios - java
+//While
+
+public static void main(String[] args) {
+        
+        int i = 150;
+        while (i < 300){
+            i++;
+            System.out.println(i);    
+        }
+        
+    }
+    
+}
