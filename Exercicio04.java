313f9f55685ee27cbd3a25afd3c8d899c087bd95
Danilo Almeida <danilo00almeida@outlook.com>
2016-12-14 20:42:07 -0200

    Usando for

---
+++ Exercicio04.java
@@ -0,0 +1,13 @@
+//Imprimindo a soma de 1 a 1000.
+
+public static void main(String[] args) {
+	
+	int soma = 0;
+
+        for (int i = 1; i <= 1000; i++){
+            soma = soma + i;            
+    	}
+        
+	System.out.println("O valor da soma e = " + soma);
+    }
+}
