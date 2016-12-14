02cc8126a0227aea83ca58272747dd04795b02f6
Danilo Almeida <danilo00almeida@outlook.com>
2016-12-14 21:10:12 -0200

    De int para long

---
+++ Exercicio07.java
@@ -0,0 +1,13 @@
+// Mudando int para long
+
+public static void main(String[] args) {
+        
+        long fatorial = 1;
+        for (long n = 1; n <= 40; n++){
+            fatorial = fatorial*n;
+            
+            System.out.println("Fatorial de "+ n + " é " + fatorial);
+            
+        }
+        
+    }
