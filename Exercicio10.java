39f1c36688654add1290c15ce07432b4b7781013
Danilo Almeida <danilo00almeida@outlook.com>
2016-12-15 19:47:08 -0200

    continuando exercicio09

---
+++ Exercicio10.java
@@ -0,0 +1,11 @@
+//Crie um método mostra(), que não recebe nem devolve parâmetro 
+//algum e simplesmente imprime todos os atributos do nosso funcionário.
+
+void mostra(){
+        System.out.println("Registro Geral: " + this.registroGeral);
+        System.out.println("Departamento: " + this.departamento);
+        System.out.println("nome: " + this.nome);
+        System.out.println("Salario R$: " + this.salario);
+        System.out.println("Data de entrada: " + this.dataEntrada);
+    }
+}
